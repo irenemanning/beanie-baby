9ac8883ee76ba9de64908c659052d7b2a690bc2e
Irene <irenemanningm@gmail.com>
2022-12-08 17:36:47 -0600

    Add Home.js component to the home folder, inside the pages folder.

---
+++ frontend/src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className="home">
+            <h3>This application is here to help you manage your beanie baby dealership.</h3>
+            <h4>View and edit your collectors and their beanie baby collections!</h4>
+        </div>
+    )
+}
+
+export default Home;
